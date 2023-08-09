b7a6f2b346fc2bd6c27cba07c4d5d0fab36f4191
mibrahim-02 <mohamed.ibrahim.aitta@gmail.com>
2023-08-09 13:11:52 +0300

    fixing new test case

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -43,7 +43,7 @@ public class TestGreeter {
   }
 
     @Test
-  public void greetShouldIncludeGreetingMessage() {
+  public void greetShouldIncludeGreetingMessage_2() {
     String someone = "World, Thanks you making me Happy";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
